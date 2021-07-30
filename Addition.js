97512c8c2bf03544ae53c1676236db5894f655cf
Kanya Bangera <kanyabangera@gmail.com>
2021-07-30 06:36:05 +0530

    Updating values in Addition.js

--- Addition.js
+++ Addition.js
@@ -1,6 +1,6 @@
 function calc_sum(){  
- var a=10;  
-var b=20;  
-var c=a+b;//It adds values of a and b variable  
-document.write(c);//prints sum of 10 and 20  
+  var a=100;  
+  var b=200;  
+  var c=a+b;//It adds values of a and b variable  
+  document.write(c);//prints sum of 10 and 20  
 }  
